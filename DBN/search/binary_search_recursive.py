7c8bd09e87d9d3cf5af8713b273e3e2c4af45831
sejigner <sejigner@gmail.com>
2023-03-06 10:58:49 +0900

    이진 탐색 알고리즘 추가

---
+++ DBN/search/binary_search_recursive.py
@@ -0,0 +1,25 @@
+# 이진 탐색 알고리즘(재귀)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    ## 중간값이 정수가 아닐 경우 버림
+    mid = (start + end) // 2
+    # 찾으면 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array[mid], target, start, mid - 1)
+    else:
+        return binary_search(array[mid], target, mid + 1, end)
+    
+# n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
+n, target = list(map(int, input().split()))
+# 전체 원소 입력받기
+array = list(map(int, input().split()))
+
+# 이진 탐색 수행 결과 출력
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
