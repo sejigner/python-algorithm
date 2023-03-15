060fc86c8fc5a7b33adcf87d01ae7090a610074d
sejigner <sejigner@gmail.com>
2023-03-15 10:41:33 +0900

    그래프 서로소 기본 알고리즘 추가

---
+++ DBN/graph/disjoint_sets.py
@@ -0,0 +1,35 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+# 부모 테이블 초기화
+for i in range(1, v + 1):
+    parent[i] = i
+
+# 두 노드를 입력 받고 union 연산 각각 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+# 각 원소가 속한 집합 출력
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+print('부모 테이블: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
