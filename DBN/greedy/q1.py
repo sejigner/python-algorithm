f92ef57f5636d92d2a6972dcb39afc1a904e049c
sejigner <sejigner@gmail.com>
2023-03-16 23:33:07 +0900

    그리디 알고리즘 기출 1번 추가

---
+++ DBN/greedy/q1.py
@@ -0,0 +1,13 @@
+n = int(input())
+data = list(map(int, input().split()))
+
+data.sort()
+result = 0
+count = 0
+for num in data:
+    count += 1
+    if count >= num:
+        result += 1
+        count = 0
+
+print(result)
